c4177d90be9f09e305326b97f21225d0dd859efa
WillThain <32332759+WillThain@users.noreply.github.com>
2018-11-13 10:42:17 +0000

    update last active not working

--- PWADemo.API/Controllers/UsersController.cs
+++ PWADemo.API/Controllers/UsersController.cs
@@ -1,25 +1,28 @@
-using PWADemo.API.Data;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
+using PWADemo.API.Data;
 using PWADemo.API.Dtos;
-using System.Collections.Generic;
+using PWADemo.API.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PWADemo.API.Controllers
 {
+    [ServiceFilter(typeof(LogUserActivity))]
     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
-        private IDemoRepository _repo;
+        private readonly IDemoRepository _repo;
         private readonly IMapper _mapper;
         public UsersController(IDemoRepository repo, IMapper mapper)
         {
             _mapper = mapper;
             _repo = repo;
-
         }
 
         [HttpGet]
@@ -32,7 +35,7 @@ namespace PWADemo.API.Controllers
             return Ok(usersToReturn);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetUser")]
         public async Task<IActionResult> GetUser(int id)
         {
             var user = await _repo.GetUser(id);
@@ -42,5 +45,20 @@ namespace PWADemo.API.Controllers
             return Ok(userToReturn);
         }
 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var userFromRepo = await _repo.GetUser(id);
+
+            _mapper.Map(userForUpdateDto, userFromRepo);
+
+            if (await _repo.SaveAll())
+                return NoContent();
+            
+            throw new Exception($"Updating user {id} failed on save");
+        }
     }
 }
--- PWADemo/src/app/routes.ts
+++ PWADemo/src/app/routes.ts
@@ -1,4 +1,3 @@
-
 import {Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
